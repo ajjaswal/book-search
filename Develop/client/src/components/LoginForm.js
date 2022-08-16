faf1234ef0544554eb9307a8319dcbc5688f60eb
AJ Jaswal <jaswalakshay@yahoo.com>
2022-08-16 14:23:21 -0400

    beginning frontend changes with loginform.js

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,8 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -10,6 +10,8 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+  // Assigns the LOGIN_USER mutation to the loginUser function and brings in the ability to error report
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -26,15 +28,13 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await loginUser({ variables: { ...userFormData }});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      Auth.login(data.login.token);
+
+      if (error) {
+        console.log(error.message);
       }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -46,7 +46,6 @@ const LoginForm = () => {
       password: '',
     });
   };
-
   return (
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
