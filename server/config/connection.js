b159967f777f232eda161a61192abb8e8ae4b423
AJ Jaswal <jaswalakshay@yahoo.com>
2022-08-20 14:19:11 -0400

    reinstalled npm packages

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-222', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
