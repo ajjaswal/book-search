fc7d618e57c194269b7a9672b8900a449df93d5d
AJ Jaswal <jaswalakshay@yahoo.com>
2022-08-20 10:48:31 -0400

    connection.js updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-222', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
